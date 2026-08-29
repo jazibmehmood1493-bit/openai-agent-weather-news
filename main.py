6fe09e9b18af71f998b07024927bb90fc0ac0e2b
jazibmehmood1493-bit <jazibmehmood1493@gmail.com>
2026-08-29 17:30:34 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-imort os
-from swarm import swarm,Agent
-client=swarm()
+import os
+from swarm import Swarm,Agent
+client=Swarm()
 def get_weather(location: str):
     return f"The weather in {location} is sunny with a temperature of 25C."
     def get_news(topic: str):
         return f"The latest news on '{topic}':openAI announces new updates!"
-        my_agent=agent(
+        my_agent=Agent(
             name="weather and News Agent",
             instructions="You are a helpful assistant.Use the available tools to provide weather updates or news.",
-            function=[get_weather,get_news],
+            functions=[get_weather,get_news],
         )
         if __name__=="__main__":
             response=client.run(
